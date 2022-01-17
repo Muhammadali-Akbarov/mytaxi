2a05041d3618027e25a524e9599f4016f15681ae
Muhammadali-Akbarov <muhammadali17abc@gmail.com>
2022-01-17 17:05:00 +0500

    database config has been changed

--- config/settings.py
+++ config/settings.py
@@ -88,17 +88,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env.str("DATABASE_NAME"),
-        'HOST': env.str("DATABASE_HOST"),
-        'PORT': env.str("DATABASE_PORT"),
-        'USER': env.str("DATABASE_USER"),
-        'PASSWORD': env.str("DATABASE_PASSWORD"),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env.str("DATABASE_NAME"),
+            'USER': env.str("DATABASE_USER"),
+            'PASSWORD': env.str("DATABASE_PASSWORD"),
+            'HOST': env.str("DATABASE_HOST"),
+            'PORT': env.str("DATABASE_PORT"),
+        }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
